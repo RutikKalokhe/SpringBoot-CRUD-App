5ef2612c2a3d9d854a8fcbb6b5b192cfa318fa69
RutikKalokhe <109789376+RutikKalokhe@users.noreply.github.com>
2023-04-12 12:09:24 +0530

    testing automatic pipeline build

--- src/test/java/com/example/firstproject/FirstProjectApplicationTests.java
+++ src/test/java/com/example/firstproject/FirstProjectApplicationTests.java
@@ -1,5 +1,5 @@
 //package com.example.firstproject;
-//
+// testing automatic pipeline build
 //import org.junit.jupiter.api.Test;
 //import org.springframework.boot.test.context.SpringBootTest;
 //
