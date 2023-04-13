ff6a46f146d1d65265dae605827a5c4b7b284bf3
RutikKalokhe <109789376+RutikKalokhe@users.noreply.github.com>
2023-04-13 12:29:10 +0530

    testing pipeline sonarqube then jenkinscicd 2nd time

--- src/test/java/com/example/firstproject/FirstProjectApplicationTests.java
+++ src/test/java/com/example/firstproject/FirstProjectApplicationTests.java
@@ -1,5 +1,5 @@
 //package com.example.firstproject;
-// testing automatic pipeline build sonarqube then jenkinscicd
+// testing automatic pipeline build sonarqube then jenkinscicd for 2nd time
 //import org.junit.jupiter.api.Test;
 //import org.springframework.boot.test.context.SpringBootTest;
 //
